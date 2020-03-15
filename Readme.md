5d64b6e1773cc3effa9847c9f92e1c980b67f8d8
Jeonghyun Lyu <app@live.jp>
2020-03-15 23:45:55 +0900

    readme
    fix readme

---
+++ Readme.md
@@ -0,0 +1,5 @@
+# 1. Project1: (Conversion) Sound -> Image (1 Model)
+
+# 2. Project2: (Conversion) Sound <-> Image Cycle (2 Models)
+
+# 3. Project3: (Conversion) Input: Sound or Image -> <SameModel> <Similar latent vector> -> output: Sound or Image (1 Model)
--- scripts/1_dataset_build.py
+++ scripts/1_dataset_build.py
@@ -7,11 +7,12 @@ from src.dataset_builder.spectrogram_helper import specgrams_to_melspecgrams
 DEFAULT_CONFIG = {
     "hop_length": 256,
     "win_length": 1024,
-    "window":"hann",
+    "window": "hann",
 }
 
+
 def expand(mat):
-    expand_vec = np.expand_dims(mat[:,-1], axis=1)
+    expand_vec = np.expand_dims(mat[:, -1], axis=1)
     expanded = np.hstack((mat, expand_vec, expand_vec))
     return expanded
 
@@ -23,23 +24,26 @@ def dataset_build(audio_path, frame_path, device="cpu"):
     audio = load_audio(path=audio_path)
     magnitude, phase = transform_stft(
         audio=audio,
-        hop_length=DEFAULT_CONFIG['hop_length'],
-        win_length=DEFAULT_CONFIG['win_length'],
-        window=DEFAULT_CONFIG['window'],
+        hop_length=DEFAULT_CONFIG["hop_length"],
+        win_length=DEFAULT_CONFIG["win_length"],
+        window=DEFAULT_CONFIG["window"],
         device="cpu",
         get_tensor=False,
     )
 
-    log_magnitude = np.log(magnitude + 1.0e-6)[:DEFAULT_CONFIG['window']]
+    log_magnitude = np.log(magnitude + 1.0e-6)[: DEFAULT_CONFIG["window"]]
     log_magnitude = expand(log_magnitude)
 
-    IF = instantaneous_frequency(angle, time_axis=1)[:DEFAULT_CONFIG['window']]
+    IF = instantaneous_frequency(angle, time_axis=1)[: DEFAULT_CONFIG["window"]]
     IF = expand(IF)
 
     assert log_magnitude.shape == (DEFAULT_CONFIG["win_length"], 128)
     assert IF.shape == (DEFAULT_CONFIG["win_length"], 128)
 
-    log_mel_magnitude_spectrograms, mel_instantaneous_frequencies = specgrams_to_melspecgrams(log_magnitude, IF)
+    (
+        log_mel_magnitude_spectrograms,
+        mel_instantaneous_frequencies,
+    ) = specgrams_to_melspecgrams(log_magnitude, IF)
 
     # Frame part
     ...
