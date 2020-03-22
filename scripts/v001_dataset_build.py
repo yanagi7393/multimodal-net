fe7dccfef60845d4e7498d63102724d586ccd8c5
Jeonghyun Lyu <app@live.jp>
2020-03-22 18:47:18 +0900

    fix videosplit to more faster

--- scripts/v001_dataset_build.py
+++ scripts/v001_dataset_build.py
@@ -15,7 +15,8 @@ DEFAULT_CONFIG = {
     "win_length": 2048,
     "window": "hann",
     "batch_size": 128,
-    "shuffle": False
+    "shuffle": False,
+    "n_jobs": 64,
 }
 
 
@@ -24,18 +25,55 @@ def expand(mat):
     expanded = np.hstack((mat, expand_vec, expand_vec))
     return expanded
 
+
 def save_files(data_dict, dirs, file_index):
     for data_type in FILENAME_TEMPLATE.keys():
-        np.save(os.path.join(dirs[data_type], FILENAME_TEMPLATE[data_type].format(file_index)), data_dict[data_type])
+        np.save(
+            os.path.join(
+                dirs[data_type], FILENAME_TEMPLATE[data_type].format(file_index)
+            ),
+            data_dict[data_type],
+        )
+
+
+def stft_to_mel(magnitude, phase):
+    # magnitude process
+    magnitude = np.squeeze(magnitude)[: DEFAULT_CONFIG["win_length"] // 2]
+    log_magnitude = np.log(magnitude + 1.0e-6)
+    log_magnitude = expand(log_magnitude)
+
+    # phase process
+    phase = np.squeeze(phase)
+    IF = instantaneous_frequency(phase, time_axis=1)[
+        : DEFAULT_CONFIG["win_length"] // 2
+    ]
+    IF = expand(IF)
+
+    # check one value
+    assert log_magnitude.shape == (DEFAULT_CONFIG["win_length"] // 2, 128)
+    assert IF.shape == (DEFAULT_CONFIG["win_length"] // 2, 128)
+
+    melspecgrams = specgrams_to_melspecgrams(log_magnitude, IF)
+    return melspecgrams
+
 
 def video_to_datasets(video_path_list, save_dir="./dataset", device="cpu"):
-    dirs = {data_type : os.path.join(save_dir, data_type) for data_type in FILENAME_TEMPLATE.keys()}
+    dirs = {
+        data_type: os.path.join(save_dir, data_type)
+        for data_type in FILENAME_TEMPLATE.keys()
+    }
 
     for _, dir in dirs.items():
         os.makedirs(dir, exist_ok=True)
 
     raw_data = RawDataset(video_path_list=video_path_list, transforms={})
-    data_loader = DataLoader(dataset=raw_data, batch_size=DEFAULT_CONFIG['batch_size'], shuffle=DEFAULT_CONFIG["shuffle"], pin_memory=True, num_workers=DEFAULT_CONFIG['batch_size'] // 2)
+    data_loader = DataLoader(
+        dataset=raw_data,
+        batch_size=DEFAULT_CONFIG["batch_size"],
+        shuffle=DEFAULT_CONFIG["shuffle"],
+        pin_memory=True,
+        num_workers=DEFAULT_CONFIG["batch_size"] // 2,
+    )
 
     i = 0
     # Audio Part
@@ -55,31 +93,38 @@ def video_to_datasets(video_path_list, save_dir="./dataset", device="cpu"):
             output_tensor=False,
         )
 
-        # None handle
-        magnitude_list = [np.squeeze(magnitude)[: DEFAULT_CONFIG["win_length"] // 2] for magnitude in magnitude_list if magnitude is not None]
-        phase_list = [np.squeeze(phase) for phase in phase_list if phase is not None]
+        stft_to_mel_params = [
+            {"magnitude": magnitude, "phase": phase}
+            for magnitude, phase in zip(magnitude_list, phase_list)
+        ]
+        melspecgrams = parallelize(
+            func=stft_to_mel, params=stft_to_mel_params, n_jobs=DEFAULT_CONFIG["n_jobs"]
+        )
 
-        log_magnitude_list = [np.log(magnitude + 1.0e-6) for magnitude in magnitude_list]
-        log_magnitude_list = [expand(log_magnitude) for log_magnitude in log_magnitude_list]
-
-        IFs = [instantaneous_frequency(phase, time_axis=1)[: DEFAULT_CONFIG["win_length"] // 2] for phase in phase_list]
-        IFs = [expand(IF) for IF in IFs]
-
-        # check one value
-        assert log_magnitude_list[-1].shape == (DEFAULT_CONFIG["win_length"] // 2, 128)
-        assert IFs[-1].shape == (DEFAULT_CONFIG["win_length"] // 2, 128)
-
-        melspecgrams_outputs = [specgrams_to_melspecgrams(log_magnitude, IF) for log_magnitude, IF in zip(log_magnitude_list, IFs)]
-        log_mel_spec_list = [item[0] for item in melspecgrams_outputs]
-        mel_if_list = [item[1] for item in melspecgrams_outputs]
+        log_mel_spec_list = [item[0] for item in melspecgrams]
+        mel_if_list = [item[1] for item in melspecgrams]
 
         # Save
         frame_list = [frame.cpu().numpy() for frame in frame_list]
         audio_list = [audio.cpu().numpy() for audio in audio_list]
 
-        params = [{"data_dict": {"frame": frame, "audio": audio, "log_mel_spec": log_mel_spec, "mel_if": mel_if}, "dirs": dirs, "file_index": i + idx} for idx, (frame, audio, log_mel_spec, mel_if) in enumerate(zip(frame_list, audio_list, log_mel_spec_list, mel_if_list))]
+        params = [
+            {
+                "data_dict": {
+                    "frame": frame,
+                    "audio": audio,
+                    "log_mel_spec": log_mel_spec,
+                    "mel_if": mel_if,
+                },
+                "dirs": dirs,
+                "file_index": i + idx,
+            }
+            for idx, (frame, audio, log_mel_spec, mel_if) in enumerate(
+                zip(frame_list, audio_list, log_mel_spec_list, mel_if_list)
+            )
+        ]
 
         # save files with parallel
-        parallelize(func=save_files, params=params, n_jobs=64)
+        parallelize(func=save_files, params=params, n_jobs=DEFAULT_CONFIG["n_jobs"])
 
         i += len(frame_list)
